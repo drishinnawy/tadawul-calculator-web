0c79d9b8366449b8a33a7b9c2337b51017cd3d24
drishinnawy <drishinnawy@gmail.com>
2025-11-25 18:37:30 +0300

    Add Arabic title to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import TadawulCalculator from "@/components/TadawulCalculator";
 
 export default function HomePage() {
   return (
-    <main className="container mx-auto py-10 px-4">
-      <TadawulCalculator />
-    </main>
+    <>
+      <Head>
+        <title>أفضل حاسبة تداول</title>
+        <meta name="description" content="أفضل حاسبة تداول لحساب الأرباح والخسائر بسهولة" />
+      </Head>
+      <main className="container mx-auto py-10 px-4">
+        <TadawulCalculator />
+      </main>
+    </>
   );
 }
