0a8c3f375ff5441fc480aa73ae2b2b9315cd0143
drishinnawy <drishinnawy@gmail.com>
2025-11-29 21:48:43 +0300

    Initial deploy: TadawulCalculator Web + GA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,25 @@
+// src/pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="ar">
+      <Head>
+        <meta charSet="UTF-8" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QWF8J7K69F"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QWF8J7K69F');
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
