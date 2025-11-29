ba3f83d297b703f20c5fa4dea9de8f87d8522994
drishinnawy <drishinnawy@gmail.com>
2025-11-30 00:14:45 +0300

    Initial commit - Tadawul Calculator with visit counter

--- src/pages/api/visits.ts
+++ src/pages/api/visits.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // أضف زيارة جديدة
   await prisma.visit.create({ data: {} });
 
