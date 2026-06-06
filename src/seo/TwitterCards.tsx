9923ebf5941544213e9de4f36d607fe630404370
drishinnawy <drishinnawy@gmail.com>
2026-06-06 13:54:12 +0300

    Add TwitterCards component for Twitter metadata

---
+++ src/seo/TwitterCards.tsx
@@ -0,0 +1,12 @@
+export default function TwitterCards() {
+  return (
+    <>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="أفضل حاسبة تداول" />
+      <meta
+        name="twitter:description"
+        content="حاسبة تداول الأسهم السعودية لحساب الربح والخسارة ومتوسط السعر."
+      />
+    </>
+  );
+}
